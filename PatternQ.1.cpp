fce7a09406d1bafbd43da8a881db0547cc6b53a8
tanmaybhardwaj2004 <116267785+tanmaybhardwaj2004@users.noreply.github.com>
2024-08-14 14:25:17 +0530

    Add files via upload

---
+++ PatternQ.1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int n=4;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i*2+1;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<i*2+1;k++)
+        {
+            cout<<"*";
+        }
+        for(int l=0;l<n-i-1;l++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
