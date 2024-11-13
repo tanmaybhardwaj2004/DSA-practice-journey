c3cb7ea2a7c41e6b3e54de0e616ab1dc8dbd9ef6
tanmaybhardwaj2004 <116267785+tanmaybhardwaj2004@users.noreply.github.com>
2024-11-13 19:05:40 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+package patterns;
+
+public class Pattern1 {
+
+	static void main(String args[])
+	{int n=5;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=0;j<i*2+1;j++)
+			{
+				System.out.print("*");
+			}
+			for(int j=0;j<n-i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
